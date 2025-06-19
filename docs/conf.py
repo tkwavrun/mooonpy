cad1e119c85ad1473e0a9337aac34d272370f124
Josh Kemppainen <jdkemppa@mtu.edu>
2025-06-19 15:39:42 -0400

    adding __init__ to mooonpy

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import sys
 import os
-sys.path.insert(0, os.path.abspath('../mooonpy'))
+sys.path.insert(0, os.path.abspath('../'))
 
 project = 'mooonpy'
 copyright = '2025, Kemppainen-Muzzy'
---
+++ mooonpy/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+# Define the __all__ variable
+__all__ = ['molspace', 'thermospace']
