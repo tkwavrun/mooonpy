1d6b10d75cd5b53c2ed2ed4ce77a4c813ed92d7e
Josh Kemppainen <jdkemppa@mtu.edu>
2025-07-03 11:41:02 -0400

    gpush.sh quick - JDK

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://sphinx-themes.org/
 
 #html_theme = 'sphinx_book_theme'
-html_theme = 'sphinx_rtd_theme'
-#html_theme = 'pydata_sphinx_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
